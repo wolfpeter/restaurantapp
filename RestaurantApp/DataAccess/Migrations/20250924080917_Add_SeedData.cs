05fa2081384fd6e9ea1674f2c41a8d8493cf93c7
Peter Wolf <wolf.peter@wopi.hu>
2025-09-24 10:31:22 +0200

    Add the missing DB migrations

---
+++ RestaurantApp/DataAccess/Migrations/20250924080917_Add_SeedData.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace RestaurantApp.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_SeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Restaurants",
+                columns: new[] { "Id", "Address", "Created", "Deleted", "Description", "Email", "Name", "PhoneNumber" },
+                values: new object[] { 1, "6722 Szeged, Indóház tér 1", new DateTime(2025, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "Teszt étterem", "info@vasutetterem.hu", "Vasút étterem", "0662123456" });
+
+            migrationBuilder.InsertData(
+                table: "MenuItems",
+                columns: new[] { "Id", "Category", "Created", "Deleted", "Description", "IsAvailable", "Name", "Price", "RestaurantId" },
+                values: new object[,]
+                {
+                    { 1, "Levesek", new DateTime(2025, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "", true, "Gyümölcsleves", 100m, 1 },
+                    { 2, "Előételek", new DateTime(2025, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "", true, "Rántott sajt", 100m, 1 },
+                    { 3, "Köretek", new DateTime(2025, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "", true, "Rizs", 100m, 1 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "MenuItems",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Restaurants",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
---
+++ RestaurantApp/DataAccess/Migrations/20250924081645_Add_default_User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantApp.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_default_User : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Address", "Created", "Deleted", "Email", "FirstName", "LastName", "PasswordHash", "PhoneNumber", "RestaurantId", "Role" },
+                values: new object[] { 1, "", new DateTime(2025, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "manager@vasutetterem.hu", "John", "Doe", "$2a$11$VoJtOj.3CALx84THrqdpruADp9ldqh16.RjH/pkbbOtdHOCk1N4gG", "0662123456", 1, "Restaurant" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
